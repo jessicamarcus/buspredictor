6cbfb84ee1bf86ffb4f85e3ad09becff252c4f3c
Jessica Marcus <jessica.marcus@gmail.com>
2014-06-06 16:29:00 -0400

    reworking in backbone

--- js/learning.js
+++ js/learning.js
@@ -19,15 +19,18 @@ var allAgencies = Backbone.Collection.extend({
     parse: function (data) {
         var parsed = [],
             agencyTitle;
-        $(data).find("agency").each(function(index) {
+        $(data).find("agency").each(function(index, value) {
+//            agencyTitle = new AgencyFactory();
             agencyTitle = $(this).attr("title");
             parsed.push({title: agencyTitle});
+//            parsed.push(agencyTitle);
         });
         return parsed;
     }
 });
 
 var allAgenciesView = Backbone.View.extend({
+    el: $('#agencyList'),
     render: function () {
             var handSource = $("#agency-template").html(),
             handTemplate = Handlebars.compile(handSource);
@@ -38,8 +41,7 @@ var allAgenciesView = Backbone.View.extend({
 $(document).ready(function () {
     var testItem = new allAgencies();
     var myView = new allAgenciesView({
-        collection: testItem,
-        el: $('#agencyList')
+        collection: testItem
     });
     myView.listenTo(testItem, "sync", function () {
         myView.render();
